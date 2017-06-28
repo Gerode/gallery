a1b882f6dd9b3de7c759d46462c922d6dfb07b98
Jay Strode <Gerode@Jays-MacBook-Pro.local>
2017-06-27 19:24:48 -0700

    buildgallery reads entire directory

--- buildgallery.js
+++ buildgallery.js
@@ -19,12 +19,12 @@ function processImage(srcKey, callback) {
     // Download the image from S3, transform, and upload to a different S3 bucket.
     async.waterfall([
         function download(next) {
-          console.log("download()");
+//          console.log("download(): " + srcKey);
           next(null, srcBucket + "/" + srcKey);
         },
             
         function transform(response, next) {
-          console.log("transform(): " + response);
+//          console.log("transform(): " + response);
           gm(response).size(function(err, size) {
             console.log(size);
               // Infer the scaling factor to avoid stretching the image unnaturally.
@@ -39,7 +39,7 @@ function processImage(srcKey, callback) {
               this.resize(width, height)
                   .toBuffer("jpg", function(err, buffer) {
                       if (err) {
-                          next(err);
+                          next("transform() " + err, response.ContentType);
                       } else {
                           next(null, response.ContentType, buffer);
                       }
@@ -47,59 +47,59 @@ function processImage(srcKey, callback) {
           });
         },
         function upload(contentType, data, next) {
-          console.log("upload(): " + contentType);
+//          console.log("upload(): " + contentType);
           fs.writeFile(dstBucket + '/' + dstKey, data);
           next(null, data);
         },
         function readXmp(data, next) {
-          console.log("readXmp(): " + srcKey);
+//          console.log("readXmp(): " + srcKey);
           gm(srcBucket + '/' + srcKey).toBuffer("XMP", next);
         },
         function extract(xmpData, next) {
-          console.log("extract(): " + srcKey);
+//          console.log("extract(): " + srcKey);
           //console.log("raw data: " + xmpData);
           xml2js.Parser().parseString(xmpData, function (err, result) {
             var caption = result['x:xmpmeta']['rdf:RDF'][0]['rdf:Description'][0]['dc:description'][0]['rdf:Alt'][0]['rdf:li'][0]["_"];
-            console.log('caption: ' + caption);
+//            console.log('caption: ' + caption);
             next(null, [srcKey, dstKey, caption]);
           });
         },
         function publish(photo, next) {
-          console.log("publish(): " + photo);
+//          console.log("publish(): " + photo);
           next(null, 
               '<figure><a href="' + photo[0] + '" class="thumbnail">\n' +
               '  <img src="' + photo[0] + '" alt="' + photo[2] + '" class="thumbnail">\n' +
               '  <figcaption>' + photo[2] + '</figcaption>\n' +
               '</a></figure>');
         }
-      ], callback
-    );
-}
-        function finalCallback(err, figure) {
+      ], 
+      callback
+/*      function finalCallback(err, figure) {
             if (err) {
                 console.error(
-//                    'Unable to resize ' + srcBucket + '/' + srcKey +
+                    'Unable to resize ' + srcBucket + '/' + srcKey +
 //                    ' and upload to ' + dstBucket + '/' + dstKey +
                     ' due to an error: ' + err
                 );
             } else {
-                console.log(
-                    'Successfully resized ' + srcBucket + '/' + figure[0] +
-                    ' and uploaded to ' + dstBucket + '/' + figure[1]
-                );
+//                console.log(
+//                    'Successfully resized ' + srcBucket + '/' + srcKey +
+//                    ' and uploaded to ' + dstBucket + '/' + figure[1]
+//                );
             }
-        }
+        }*/
+    );
+}
 
-images = [
-  "2016_13590481_1942374482655774_3308457104912371305_n.jpg",
-  "2016_13584872_10210502083016149_2338582507920623038_o.jpeg",
-  "2016_13653220_10210502059015549_3570443696251764115_o.jpeg"];
-
-async.map(images, processImage, function(err, result) {
-  if (err) {
-    console.error('map() error: ' + err);
-  }
-  else {
-    console.log('<div id="gallery">\n' + result.join('\n') + '\n</div>');
-  }
+fs.readdir(srcBucket, null, function (err, images) {
+  async.map(images.filter(function(srcKey) {return srcKey.startsWith('2016') && srcKey.endsWith('g');}), processImage, function(err, result) {
+    console.log('map() callback');
+    if (err) {
+      console.error('map() error: ' + err);
+    }
+    else {
+      console.log('<div id="gallery">\n' + result.join('\n') + '\n</div>');
+    }
+  });
 });
+
