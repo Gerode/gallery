7320466ce9094ff7ed9c1e788b980dce11911948
Jay Strode <Gerode@Jays-MacBook-Pro.local>
2017-07-29 18:42:08 -0700

    Initial index pages

--- buildgallery.js
+++ buildgallery.js
@@ -75,29 +75,37 @@ function processImage(s3Object, callback) {
             }
             });
 
+          function publish(caption, next) {
+//              console.log("publish(): " + caption);
+            next(null, 
+                '<figure><a href="/' + srcKey + '" class="thumbnail">\n' +
+                '  <img src="/' + dstKey + '" alt="' + caption + '" class="thumbnail">\n' +
+                '  <figcaption>' + caption + '</figcaption>\n' +
+                '</a></figure>');
+          };
+
           async.waterfall([
             function readXmp(next) {
 //              console.log("readXmp(): " + srcKey);
               gm(response.Body).toBuffer("XMP", next);
             },
             function extract(xmpData, next) {
-    //          console.log("extract(): " + srcKey);
+//              console.log("extract(): " + srcKey);
               //console.log("raw data: " + xmpData);
               xml2js.Parser().parseString(xmpData, function (err, result) {
                 var caption = result['x:xmpmeta']['rdf:RDF'][0]['rdf:Description'][0]['dc:description'][0]['rdf:Alt'][0]['rdf:li'][0]["_"];
-    //            console.log('caption: ' + caption);
-                next(null, [srcKey, dstKey, caption]);
+                next(null, caption);
               });
-            },
-            function publish(photo, next) {
-    //          console.log("publish(): " + photo);
-              next(null, 
-                  '<figure><a href="/' + photo[0] + '" class="thumbnail">\n' +
-                  '  <img src="/' + photo[1] + '" alt="' + photo[2] + '" class="thumbnail">\n' +
-                  '  <figcaption>' + photo[2] + '</figcaption>\n' +
-                  '</a></figure>');
             }],
-            outernext
+            function(err, result) {
+              if (err) {
+                console.error('process() ' + srcKey + ' metadata error: ' + err);
+                publish('', outernext);
+              }
+              else {
+                publish(result, outernext);
+              }
+            }
           );
         },
             
@@ -106,30 +114,60 @@ function processImage(s3Object, callback) {
     );
 }
 
+function htmlNav(pages) {
+  return '<h2><a href="/">Home</a>' + pages.map(function(page, ndx) {
+    return ' > <a href="/' + pages.slice(0, ndx+1).join('/') + '">' + page + '</a>';
+  }).join('') + '</h2>';
+}
+
+function htmlCopyright() {
+  return '<br><div id="copyright"><a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.</div>';
+}
+
+function generateGallery(galleryInfo, callback) {
+  var galleryYr = galleryInfo[0];
+  var galleryMo = galleryInfo[1];
+
+  async.waterfall([
+    function listObjects(next) {
+      s3.listObjectsV2({Bucket: srcBucket, MaxKeys: 20, Prefix: galleryYr + '/' + galleryMo + '/IMG'}, next);
+    },
+    function processImages(images, next) {
+//      console.log("processImages(): " + images['Contents']);
+      async.map(images['Contents'].filter(function(object) {return object['Key'].endsWith('g');}), processImage, next);
+    },
+    function createIndex(result, next) {
+      s3.putObject(
+        {
+          Bucket: dstBucket,
+          Key: galleryYr + '/' + galleryMo + '/index.html',
+          Body: '<html><head><link rel="stylesheet" href="/gallery.css"/></head><body><div id="gallery">' + htmlNav([galleryYr, galleryMo]) + '<h1>' + galleryMo + '</h1>\n' + result.join('\n') + '\n' + htmlCopyright() + '</div></body></html>',
+          ContentType: 'text/html'
+        },
+        next);
+    }],
+    function(err, result) {
+      if (err) {
+        console.error('generateGallery() ' + galleryYr + '/' + galleryMo + ' error: ' + err);
+      }
+      else {
+        callback(null, galleryYr + '/' + galleryMo);
+      }
+    }
+  );
+}
+
 async.waterfall([
-  function listObjects(next) {
-    s3.listObjectsV2({Bucket: srcBucket, MaxKeys: 20}, next);
+  function generateGalleries(next) {
+    async.map([['2013', '05'], ['2014', '02']], generateGallery, next);
   },
-  function processImages(images, next) {
-    console.log("processImages(): " + images);
-    async.map(images['Contents'].filter(function(object) {return object['Key'].startsWith('2013/2013-05/IMG') && object['Key'].endsWith('g');}), processImage, next);
-  },
-  function createIndex(result, next) {
-    s3.putObject(
-      {
-        Bucket: dstBucket,
-        Key: '2013/2013-05/index.html',
-        Body: '<html><head><link rel="stylesheet" href="/gallery.css"/></head><body><div id="gallery"><h2><a href="/">Home</a> > <a href="2013">2013</a> > <a href="2013-05">05</a></h2><h1>2013-05</h1>\n' + result.join('\n') + '\n<br><div id="copyright"><a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.</div></div></body></html>',
-        ContentType: 'text/html'
-      },
-      next);
-  },
-  function createRootIndex(result, next) {
+  function createRootIndex(galleries, next) {
+    console.log("createRootIndex(): " + galleries);
     s3.putObject(
       {
         Bucket: dstBucket,
         Key: 'index.html',
-        Body: '<html><head><link rel="stylesheet" href="/gallery.css"/></head><body>Hello, world!</body></html>',
+        Body: '<html><head><link rel="stylesheet" href="/gallery.css"/></head><body><div id="gallerydir">' + htmlNav([]) + galleries.map(function(gallery) {return '<div><a href="' + gallery + '">' + gallery + '</a></div>'}).join('\n') + htmlCopyright() + '</div></body></html>',
         ContentType: 'text/html'
       },
       next);
@@ -146,7 +184,7 @@ async.waterfall([
   }],
   function(err, result) {
     if (err) {
-      console.error('putObject() index  error: ' + err);
+      console.error('top-level error: ' + err);
     }
   }
 );
